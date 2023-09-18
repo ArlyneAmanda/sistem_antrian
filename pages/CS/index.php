ee6d0a9bb9111c9770ff7126e282216c41785186
ArlyneAmanda <arlin.amanda12@gmail.com>
2023-09-18 09:13:32 +0700

    mengubah bnyak

--- pages/CS/index.php
+++ pages/CS/index.php
@@ -128,7 +128,6 @@
         </div>
     </div>
 
-  <img src="../../images/CSpics.webp" alt="" class="img1">
     <!-- Include Bootstrap JS and jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
